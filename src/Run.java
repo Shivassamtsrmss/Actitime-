a7c57ed5627f673adb2d11783f31d55a3a917c0d
CSC <CSC@DESKTOP-OVFN53Q>
2023-11-26 00:23:32 +0530

    Shree sree Balaji

---
+++ src/Run.java
@@ -0,0 +1,12 @@
+
+public class Run {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("jai shree sita ram anjanay govinda gnapathi deva");
+
+	}
+
+}
